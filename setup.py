3f0839115a6bc7c3ffa2600a90e2f520bc06fa36
Corbin McNeill <corbin.mc96@gmail.com>
2020-06-23 14:58:44 -0400

    reformatting

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="smith-normal-from-corbinmcneill", # Replace with your own username
+    name="smith-normal-from-corbinmcneill",
     version="0.0.1",
     author="Corbin McNeill",
     author_email="corbin.mc96@gmail.com",
-    description="A tool for computing the Smith Normal Forms over arbitrary Principle Ideal Domains",
+    description=("A tool for computing the Smith Normal Forms " +
+                 "over arbitrary Principle Ideal Domains"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/corbinmcneill/snf",
@@ -20,4 +21,3 @@ setuptools.setup(
     ],
     python_requires='>=3',
 )
-
