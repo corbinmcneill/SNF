33d5145f48836f3122c9a1770ef289a4d12da58c
Corbin McNeill <corbin.mc96@gmail.com>
2020-06-23 11:54:40 -0400

    changed example code

--- example.py
+++ example.py
@@ -2,14 +2,22 @@ from snf import matrix
 from snf import snfproblem
 from snf import z
 from snf import zi
+import random as rand
 
-snfMatrix = matrix.Matrix(2, 2, [z.Z(i) for i in range(1,5)])
-snfProb = snfproblem.SNFProblem(snfMatrix)
+
+rand.seed(1002)
+
+snfContents = [rand.randrange(-10, 10) for _ in range(4)]
+snfMatrix = matrix.Matrix(2, 2, [z.Z(x) for x in snfContents])
+snfProb = snfproblem.SNFProblem(snfMatrix, debug=True)
 snfProb.computeSNF()
 
 print(f"A:\n{snfProb.A}")
 print(f"J:\n{snfProb.J}")
 print(f"S:\n{snfProb.S}")
 print(f"T:\n{snfProb.T}")
+
+print(f"S.det: {snfProb.S.determinant()}")
+print(f"T.det: {snfProb.T.determinant()}")
 
 print(f"S*A*T == J ?: {snfProb.S * snfProb.A * snfProb.T == snfProb.J}")
