947e8f5ec9be787dc3bec0b5509ea6568aa2f142
Corbin McNeill <corbin.mc96@gmail.com>
2021-01-13 22:26:55 -0700

    added python 3.9 support

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
